b4afa341b4b94b143e813959a0f1a91b0118611c
sabrina2712 <mailroshni@gmail.com>
2020-07-27 14:45:23 +0200

    add colors state

--- src/components/mainMap.js
+++ src/components/mainMap.js
@@ -21,15 +21,74 @@ import {get as getProjection} from 'ol/proj';
 
 
   class MainMap extends React.Component{
+constructor(){
+  super();
+  this.state ={stateRevenue : {
+    "DE-BB": 5627,
+    "DE-BE": 5500,
+    "DE-BW": 3231,
+    "DE-BY": 1813,
+    "DE-HB": 9792,
+    "DE-HE": 4538,
+    "DE-HH": 2883,
+    "DE-MV": 9752,
+    "DE-NI": 5550,
+    "DE-NW": 282,
+    "DE-RP": 1469,
+    "DE-SH": 9673,
+    "DE-SL": 7858,
+    "DE-SN": 3835,
+    "DE-ST": 3852,
+    "DE-TH": 4264
+    }}
+}
+     
+componentDidMount(){
 
-  componentDidMount(){
-    var jsondata = JSON.parse(data);
-    
+      const resultRev = data.features.map((f)=>{
+      const stateOfThisFeature = f.properties['id'];
+      const revenueForThisState = this.state.stateRevenue[stateOfThisFeature];
+      f.properties['revenue'] = revenueForThisState;
+      return revenueForThisState;
+      });
+      
+    console.log(resultRev)
+ 
+      const colorFunc = resultRev.map((result, i)=>{
+              console.log(result)
+              let color = null;
+             if (result > 0 && result < 2000) {
+              console.log(result)
+              color = 'rgba(255,0,0,0.2)';
+        
+            } else if (result >= 2000 && result < 4000) {
+              console.log(result)
+              color = 'rgba(0,0,255,0.2)';
+            } else if (result > 4000) {
+              console.log(result)
+              color = 'rgba(0,255,0,0.4)';
+            }
+             
+             return  new Style({
+                stroke: new Stroke({
+                  color: 'green',
+                  width: 2
+                }),
+                fill: new Fill({
+                  color: color, 
+                  
+                })
+              })
+     
 
-
-
-
-    var vectorSource = new VectorSource({
+        })
+     
+        
+ 
+       
+      
+  
+var vectorSource = new VectorSource({
         features: new GeoJSON({
         dataProjection: "EPSG:4326",
         featureProjection:"EPSG:3857"
@@ -38,7 +97,7 @@ import {get as getProjection} from 'ol/proj';
 
 
 
-      let vectorLayer = new VectorLayer({ source: vectorSource });
+      let vectorLayer = new VectorLayer({ style: colorFunc,source: vectorSource });
      
       var map = new Map({
             target: 'map',
