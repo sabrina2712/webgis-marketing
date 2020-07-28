3d051db611ee1419456b6eab53f4ff3ecae442b9
sabrina2712 <mailroshni@gmail.com>
2020-07-28 12:30:04 +0200

    added pop up info

--- src/components/mainMap.js
+++ src/components/mainMap.js
@@ -5,120 +5,181 @@ import Feature from 'ol/Feature';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import GeoJSON from 'ol/format/GeoJSON';
-import { Circle, Point } from 'ol/geom';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import { Circle as CircleStyle, Fill, Stroke, Style, Icon, RegularShape } from 'ol/style';
-import Overlay from 'ol/Overlay';
-import { toLonLat } from 'ol/proj';
 import { fromLonLat, get } from "ol/proj"
-import { transform } from 'ol/proj';
-import { toStringHDMS } from 'ol/coordinate';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
 import data from "./germany.json"
-import Polygon from 'ol/geom/Polygon';
-import {get as getProjection} from 'ol/proj';
+import Overlay from 'ol/Overlay';
 
 
-  class MainMap extends React.Component{
-constructor(){
-  super();
-  this.state ={stateRevenue : {
-    "DE-BB": 5627,
-    "DE-BE": 5500,
-    "DE-BW": 3231,
-    "DE-BY": 1813,
-    "DE-HB": 9792,
-    "DE-HE": 4538,
-    "DE-HH": 2883,
-    "DE-MV": 9752,
-    "DE-NI": 5550,
-    "DE-NW": 282,
-    "DE-RP": 1469,
-    "DE-SH": 9673,
-    "DE-SL": 7858,
-    "DE-SN": 3835,
-    "DE-ST": 3852,
-    "DE-TH": 4264
-    },
-  colors: {red: "rgba(255,0,0,1)", green: "rgba(0,255,0,1)", blue:"rgba(0,0,255,1)", purple: "rgba(145, 61, 136, 1)" }}
+
+
+class MainMap extends React.Component{
+    constructor(){
+        super();
+        this.state ={stateRevenue : {
+          "DE-BB": 5627,
+          "DE-BE": 5500,
+          "DE-BW": 3231,
+          "DE-BY": 1813,
+          "DE-HB": 9792,
+          "DE-HE": 4538,
+          "DE-HH": 2883,
+          "DE-MV": 9752,
+          "DE-NI": 5550,
+          "DE-NW": 282,
+          "DE-RP": 1469,
+          "DE-SH": 9673,
+          "DE-SL": 7858,
+          "DE-SN": 3835,
+          "DE-ST": 3852,
+          "DE-TH": 4264
+          },
+        colors: {red: "rgba(255,0,0,1)", green: "rgba(0,255,0,1)", blue:"rgba(0,0,255,1)", purple: "rgba(145, 61, 136, 1)" },
+        info: ""
+      }
+        
 }
+    onClickHandler=(evt)=>{
      
-componentDidMount(){
+      let pixel = evt.pixel;
+           console.log("clicked")
+           let stateRev =this.state.stateRevenue
+           console.log(this)
+          this.map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+           
+                  let coordinateClicked = evt.coordinate;
+                  let eachId = feature.get("id")
+                  console.log(stateRev)
+                  let curRev = stateRev[eachId]
+                  console.log(curRev)
+                  this.setState(()=>{
+                    let info =this.state.info;
+                    return {info: curRev}
+                  })
+                
 
-      const resultRev = data.features.map((f)=>{
+              });}
+    
+    
+ componentDidMount(){
+
+      data.features.forEach((f)=>{
       const stateOfThisFeature = f.properties['id'];
       const revenueForThisState = this.state.stateRevenue[stateOfThisFeature];
       f.properties['revenue'] = revenueForThisState;
-      return revenueForThisState;
+      
       });
-
-    console.log(resultRev)
-
       const getColor = (d)=>{
-        console.log(d)
-      return d > 0 ? '#800026' :
-        d > 1000  ? '#BD0026' :
-        d > 2000  ? '#E31A1C' :
+       
+      return d > 6000 ? '#800026' :
+        d > 5000  ? '#BD0026' :
+        d > 4000  ? '#E31A1C' :
         d > 3000  ? '#FC4E2A' :
-        d > 4000   ? '#FD8D3C' :
-        d > 5000   ? '#FEB24C' :
-        d > 6000 ? '#FED976' :
+        d > 2000   ? '#FD8D3C' :
+        d > 1000   ? '#FEB24C' :
+        d > 500 ? '#FED976' :
                    '#FFEDA0';
       }
-             
-      function getStyle(data) {
-      
-        return  new Style({
-           stroke: new Stroke({
-            
-             width: 2
-           }),
-           fill: new Fill({
-             color: getColor( data.features.map((f)=>{
-               console.log(f)
-                   return f.get("properties").s;
-             }))
-             
-             
-           })
-         })
+          
+     
 
-       
-    }
-      
-      
-var vectorSource = new VectorSource({
+
+
+      const getStyle=(f)=> {
+        let id = f.get("id")
+        let name = f.get("name")
+        let rev = this.state.stateRevenue[id]
+        
+        console.log(rev)
+          return  new Style({
+          stroke: new Stroke({
+          width: 2
+          }),
+          fill: new Fill({
+          color: getColor( f.get("revenue"))
+        })
+     })}
+     
+
+
+
+var infoMy = document.getElementById('info');
+
+
+/**
+ * Create an overlay to anchor the popup to the map.
+ */
+var overlay = new Overlay({
+	element: infoMy,
+	autoPan: true,
+	autoPanAnimation: {
+		duration: 250
+	}
+});
+            
+    var vectorSource = new VectorSource({
         features: new GeoJSON({
-        dataProjection: "EPSG:4326",
-        featureProjection:"EPSG:3857"
+            dataProjection: "EPSG:4326",
+            featureProjection:"EPSG:3857"
         }).readFeatures(data),
         });
 
-
-
-      let vectorLayer = new VectorLayer({ style:  getStyle, source: vectorSource });
-     
-      var map = new Map({
-            target: 'map',
-            layers: [
-              new TileLayer({
-                source: new OSM()
+   let vectorLayer = new VectorLayer({ style:  getStyle, source: vectorSource });
+        var map = new Map({
+              target: 'map',
+              layers: [
+            new TileLayer({
+              source: new OSM()
               }),   
-                vectorLayer
-              ],
-                view: new View({
-                center: fromLonLat([0, 0]),
-                zoom: 4
+            vectorLayer
+            ],
+            overlays: [overlay],
+              view: new View({
+              center: fromLonLat([ 13.404954, 52.520008]),
+              zoom: 5
               })
             });
+      let stateRev =this.state.stateRevenue
+      let info = this.state.info
+    
+    
+      map.on('click', function(evt) {
+              
+        let pixel = evt.pixel;
+        console.log("clicked")
         
-          }
+        console.log(pixel)
+        console.log(this)
+     map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+      
+      let stateName = feature.get("name")
+      console.log(stateName)
+             let eachId = feature.get("id")
+             console.log(stateRev)
+             let curRev = stateRev[eachId]
+            
+             console.log(this)
+             var coordinate = evt.coordinate;
+             overlay.setPosition(coordinate);
+            
+     let info = document.getElementById("info")
+     info.innerHTML= ` ${ stateName} Rv: ${curRev}`
+          })
+         
+        })
+      }
+      
+     
+
+         
+       
           render(){
-            return(<div id="map" className="main-map"></div>)
+            return(<div id="map" className="main-map" >
+              <div id="info" ></div>
+          
+            </div>)
           }
-
-  }
-
-
+        }
 export default MainMap;
